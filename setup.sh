817b0ad1787729c929efc5da1f068ff0e6d9e668
Brian Spiering <bspiering@gmail.com>
2018-03-03 10:54:45 -0800

    Minor revisions

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ source activate word
 # Update environment (might break stuff. move fast!?)
 conda update --all --yes
 
+# Setup spell checking and other notebook enhancements
+git clone https://github.com/Calysto/notebook-extensions.git
+cd notebook-extensions
+jupyter nbextension install calysto --user
+jupyter nbextension enable calysto/spell-check/main
+jupyter nbextension enable calysto/cell-tools/main
+jupyter nbextension enable calysto/annotate/main
+rm -r -f notebook-extensions
+
 # Setup RISE (https://github.com/damianavila/RISE) slideshows 
 jupyter nbextension install rise --py --sys-prefix
 jupyter nbextension enable rise --py --sys-prefix
